778b3f5e482696d2211f921099a986eef24f9839
Alexsandr Kart <sakart47@mail.ru>
2022-03-03 17:08:22 +0300

    Решение дз2

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp
 {
@@ -49,33 +50,120 @@ namespace ConsoleApp
             }
 
             void task1(){
-               
+                // В пайтоне в 1 строку =) print(str(input("Введите число"))[::-1])  
+               Console.WriteLine("Ведите число");
+            //    Вариант1
+            //    string stringNumer = Console.ReadLine();
+            //    char [] number = stringNumer.ToCharArray();
+            //    Array.Reverse(number);
+            //    stringNumer = new string(number);
+            //    Console.WriteLine(stringNumer);
+
+            //    Вариант2
+            string stringNumer = Console.ReadLine();
+            for(var i = stringNumer.Length - 1 ; i >= 0; i--){
+                Console.Write(stringNumer[i]);
+            }
 
             };
 
             void task2(){
-               
+               Console.WriteLine("Ведите число");
+               int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+               if(number>99){
+                   string numberString = number.ToString();
+                   Console.WriteLine($"3- число = {numberString[2]}");
+               }else{
+                    Console.WriteLine("3-го числа нет");
+               }
             };
 
             void task3(){
-           
+                Console.WriteLine("Укажите день недели, цифрой от 1 до 7, где 1 - это понедельник, а 7 -Воскресенье");
+               int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+               if(number == 6 || number == 7){               
+                   Console.WriteLine("Ура, сегодня выходной");
+               }else if(number >= 1 && number < 6){
+                   Console.WriteLine("Эххх, сегодня на работу");
+               }else{
+                   Console.WriteLine("День недели указан неправильно");
+               }
 
             };
 
             void task4(){
-              
+
+                Console.WriteLine("Ведите число длину стороны a");
+                int side_a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите число длину стороны b");
+                int side_b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите число длину стороны c");
+                int side_c = Convert.ToInt32(Console.ReadLine());
+                if(side_a == side_b || side_b == side_c || side_a == side_c){
+                    Console.WriteLine("Треугодбник равнобедренный");
+                }else{
+                    Console.WriteLine("Треугодбник не равнобедренный");
+                }
                 }
 
                
             
             void task5(){
-              
+                //вариант1
+                Console.WriteLine("вариант1");
+                string rePattern = @"\d{2}\.\d{2}\.\d{4}";
+
+                Console.WriteLine("Ведите дату рождения в формате дд.мм.гггг");
+                string birthDate = Console.ReadLine();
+                string [] matches = Regex.Matches(birthDate, rePattern, RegexOptions.IgnoreCase)
+                                                .Cast<Match>()
+                                                .Select(m => m.Value)
+                                                .ToArray();
+                if(matches.Length == 1){
+                    // Проверяем на дату 01.02.2022 при том если др выпадает на 1 февраля, то считаем его как исполнившееся
+                    string [] dates = matches[0].Split('.');
+                   
+                    if(Convert.ToInt32(dates[1]) < 2){
+                        Console.WriteLine($"Возраст {2022-Convert.ToInt32(dates[2])}");
+                    }else if(Convert.ToInt32(dates[1]) > 2){
+                        Console.WriteLine($"Возраст {2022-Convert.ToInt32(dates[2])-1}");
+                    }
+                }else{
+                    Console.WriteLine("Вы ввели дату в неправильном формате");
+                }
+
+                //вариант2 
+                // Console.WriteLine("вариант2");
+                // DateTime dateNow = new DateTime(2022,2,1);
+
+                // Console.WriteLine("Ведите дату рождения в формате дд.мм.гггг");
+                // string birthDate = Console.ReadLine();
+                // DateTime birthDate2 = DateTime.ParseExact(birthDate, "dd.MM.yyyy", System.Globalization.CultureInfo.InvariantCulture);
+                // TimeSpan timeDelta= dateNow.Subtract(birthDate2) ;
+                // //Console.WriteLine(timeDelta.);
+
+                // Console.WriteLine(birthDate2.ToString());
+
                 }
 
                
             
             void task6(){
-              
+                    double procRecFunc(int mouth, double summ){
+                        if(mouth>1){
+                            summ += summ * 0.015;
+                            mouth--;
+                            return procRecFunc(mouth,summ);
+                        }else{
+                            summ += summ * 0.015;
+                            return summ;
+                        }
+                    }
+                    Console.WriteLine("Ведите Кол-во месяцев");
+                    int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+                    int capital = Convert.ToInt32(procRecFunc(number, 1000.0));
+                    Console.WriteLine($"Сумма из 1000р, через {number} месяцев будет составлять {capital}");
+
                 }
 
                
