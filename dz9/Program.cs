dd264abf816c4732f9b929b34ce3d45c0a5c9536
Alexsandr Kart <sakart47@mail.ru>
2022-04-13 18:54:19 +0300

    dz9

---
+++ dz9/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine(@"Какую задачу запустить, укажите номер: 
+            1.  Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+            2.  Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+            3.  Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+            4.  Дано предложение. Напишите рекурсивный метод, подсчитывающий количество слов в данном предложении. Словом считается последовательность символов без пробелов.
+            5.  Известно, что пароль длиной 3 символа состоит из латинских букв строчного регистра и цифр от 0 до 9. Напишите рекурсивный метод, который перебирает все комбинации паролей. 
+            6.  Даны натуральные числа a и b. Рекурсивно описать функцию возведения числа a в степень b, используя только операцию инкрементирования (“++”).
+            ");
+            int num = Convert.ToInt32(Console.ReadLine()) ;
+            switch (num)
+            {
+                case 1:
+                    task1();
+                    break;
+                case 2:
+                    task2();
+                    break;
+                case 3:
+                    task3();
+                    break;
+                case 4:
+                    task4();
+                    break;
+                case 5:
+                    task5();
+                    break;
+                case 6:
+                    task6();
+                    break;
+                
+                default:
+                    Console.WriteLine("Вы ввели неправильное число");
+                    break;
+            }
+
+            //Функция получения начального и конечного числа ряда
+            int[] AskUser(){
+                int[] period = new int[2];
+                Console.Write("Ведите начальное число: ");
+                period[0] = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("");
+                Console.Write("Ведите Конечное число: ");
+                period[1] = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("");
+
+                return period;
+            }
+           
+
+
+
+             void task1(){
+                int[] period = AskUser();
+                
+                for(int i = period[0]; i <= period[1]; i++){
+                    if(i%3 == 0){
+                       Console.Write($" {i},");   
+                    }
+                }
+            };
+
+             void task2(){
+                int[] period = AskUser();
+                int sum = 0;
+                for(int i = period[0]; i <= period[1]; i++){
+                    sum += i;
+                }
+                 Console.Write($" {sum}");
+               
+                
+            };
+
+             void task3(){
+                int akkerman(int m, int n){
+                    Console.WriteLine($" {m}, {n}");
+                    if(m==0){
+                        return n+1;
+                    }else if(n==0){
+                        return akkerman(m-1,1);
+                    }
+                    else{
+                        return akkerman(m-1,akkerman(m,n-1));
+                    }
+                }
+                int m = 3;
+                int n = 2;
+                Console.WriteLine(akkerman(m,n));
+            };
+
+             void task4(){
+                int countString(string str, int step=0){
+                    if(step == str.Length-1){
+                        return 1;
+                    }else if(str[step] == ' ' && str[step-1] != ' '){
+                        return 1+countString(str,step+1);
+                    }else{
+                        return countString(str,step+1);
+                    }
+
+                }
+                string str = @"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+                
+
+                Console.WriteLine($"Кол-во слов = {countString(str)}");
+               
+
+            };
+            
+             void task5(int lenPassworn = 3){
+                string dictionaryABC = "0123456789abcdefghijklmnopqrstuvwxyz";
+                string GenerationPass(int len){
+                    Random rnd = new Random(); 
+                    string pass = "";
+                    for(int i = 0; i < len; i++){
+                        
+                        pass += dictionaryABC[rnd.Next(0,dictionaryABC.Length)];
+                    }
+                    return pass;
+                }
+                string convertPass(int step){
+                    // Конвектируем порядковый номер в строку
+                    int lenDictionary = dictionaryABC.Length;
+                   
+                    if(step < lenDictionary){
+                        return $"{dictionaryABC[step]}";
+                    }else{
+                        return convertPass(step/(lenDictionary)) + dictionaryABC[step%lenDictionary];
+                    }
+                    
+
+                };
+                string BruteForcePass(string secretPass, int step=0){
+                    //рекурсивная фукция брутфорса пароля
+
+                    
+                    int countCombination = Convert.ToInt32(Math.Pow(dictionaryABC.Length-1, lenPassworn));
+
+                    if(step == countCombination-1){
+                        return "не нашли";
+                    }else if(convertPass(step) == secretPass){
+                        return convertPass(step);
+                    }else{
+                        Console.WriteLine($"пароль {convertPass(step)}");
+                        try{
+                            return BruteForcePass(secretPass,step+1);
+                        }
+                        catch {
+                            return("Ошибка");
+                            }
+                        
+                    }
+
+
+                    return "";
+                }
+
+                int countCombination = Convert.ToInt32(Math.Pow(dictionaryABC.Length, lenPassworn));
+
+                string pass = GenerationPass(lenPassworn);
+                
+
+                try{
+                    Console.WriteLine($"Угадали пароль {BruteForcePass(pass)}");
+                }catch{
+                    Console.WriteLine($"Ошибка");
+                }
+              
+                
+              
+              
+            };
+
+             void task6(){
+                int recursDegree(int number, int degree){
+                    if(degree == 1){
+                        return number;
+                    }
+                    return number*recursDegree(number,degree-1);
+                }
+
+                int a = 2;
+                int b = 23;
+                Console.WriteLine(recursDegree(a,b));
+
+               
+            };
+
+           
+               
+            
+           }
+
+
+
+               
+            
+        
+
+        
+    }
+}
